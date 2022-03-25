08f01c7e96b7ddbb692e74f2112b3ed01b3c3dec
Wt <1105889424@qq.com>
2022-03-25 13:28:04 +0800

    Toaday

---
+++ exp1/AircraftWar-base/src/edu/hitsz/props/NormalProFactory.java
@@ -0,0 +1,18 @@
+package edu.hitsz.props;
+
+public class NormalProFactory extends PropsFactory {
+    @Override
+    AbstractProps createProps(String type, int x, int y) {
+        AbstractProps props = null;
+        if (type.equals("bomb")) {
+            props = new BombProps(x, y, 0, 2);
+
+        } else if (type.equals("bullet")) {
+            props = new BulletProps(x, y, 0, 2);
+        } else if (type.equals("blood"))
+            props = new BloodProps(x, y, 0, 2);
+
+        return props;
+    }
+
+}
